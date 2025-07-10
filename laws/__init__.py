1ab97f2430e3370c9e9ac05e292aea7d89652faf
Shaikh Umer <n0sync.76@gmail.com>
2025-07-10 22:17:51 +0530

    feat: add conservation laws module

---
+++ laws/__init__.py
@@ -0,0 +1,13 @@
+from . import momentum
+from . import work_energy
+from . import conservation
+from . import newtons_laws
+from . import electromagnetic
+
+__all__=[
+    'momentum',
+    'work_energy',
+    'conservation',
+    'newtons_laws',
+    'electromagnetic'
+]
