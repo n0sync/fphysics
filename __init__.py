5cbb92739b4a8799007e5d9797171c98250bff57
Shaikh Umer <n0sync.76@gmail.com>
2025-07-19 02:26:09 +0530

    refactor: __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,19 @@
 from . import constants
 from . import mechanics
+from . import laws
+from . import oscillations
+from . import thermodynamics
+from . import statistical_mechanics
+from . import electromagnetism
+from . import utils
 
 __all__ = [
     "constants",
     "mechanics",
-    "utils"
+    "laws",
+    "oscillations",
+    "thermodynamics",
+    "statistical_mechanics",
+    "electromagnetism",
+    "utils",
 ]
