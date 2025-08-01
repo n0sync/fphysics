1ba1f88addfc2391f8c87376629c4940f93ca23e
Shaikh Umer <n0sync.76@gmail.com>
2025-08-02 01:24:08 +0530

    refactor: init file

--- laws/__init__.py
+++ laws/__init__.py
@@ -1,13 +1,1 @@
-from . import momentum
-from . import work_energy
-from . import conservation
-from . import newtons_laws
-from . import electromagnetic
 
-__all__=[
-    'momentum',
-    'work_energy',
-    'conservation',
-    'newtons_laws',
-    'electromagnetic'
-]
