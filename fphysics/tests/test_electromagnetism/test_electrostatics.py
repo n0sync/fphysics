61577b0fa2c5d2496e0e21adb84eb1b0bd4541c8
Umer Shaikh <n0sync.76@gmail.com>
2025-09-23 01:24:27 +0530

    refactor test_electrostatics.py

--- fphysics/tests/test_electromagnetism/test_electrostatics.py
+++ fphysics/tests/test_electromagnetism/test_electrostatics.py
@@ -1,28 +1,21 @@
-"""
-Test electromagnetism electrostatics module.
-"""
-
 import unittest
 import sys
 import os
-from constants import COULOMB_CONSTANT, ELEMENTARY_CHARGE
+from ..constants import COULOMB_CONSTANT, ELEMENTARY_CHARGE
 
 class TestElectrostatics(unittest.TestCase):
-    """Test electrostatics functions"""
     
     def test_coulomb_force(self):
-        """Test Coulomb's law force calculation"""
         charge1 = ELEMENTARY_CHARGE
         charge2 = ELEMENTARY_CHARGE
-        distance = 1e-10  # 1 Angstrom
+        distance = 1e-10
         
         force = COULOMB_CONSTANT * charge1 * charge2 / distance**2
         self.assertGreater(force, 0)
     
     def test_electric_field(self):
-        """Test electric field calculation"""
         charge = ELEMENTARY_CHARGE
-        distance = 1e-9  # 1 nm
+        distance = 1e-9
         
         electric_field = COULOMB_CONSTANT * charge / distance**2
         self.assertGreater(electric_field, 0)
