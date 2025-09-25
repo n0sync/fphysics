0386161ea373fa081873a37a4eb561d3c9370bce
Umer Shaikh <n0sync.76@gmail.com>
2025-09-26 02:41:08 +0530

    refactor: add standalone functions

--- fphysics/tools/calculators.py
+++ fphysics/tools/calculators.py
@@ -1,20 +1,26 @@
-from ..constants import GRAVITATIONAL_CONSTANT, ELEMENTARY_CHARGE
+from ..constants import GRAVITATIONAL_CONSTANT, COULOMB_CONSTANT
 
-class PhysicsCalculator:
-    def calculate_force_gravity(self, mass1, mass2, distance):
-        return GRAVITATIONAL_CONSTANT * (mass1 * mass2) / distance**2
+def calculate_gravitational_force(mass1, mass2, distance):
+    if distance <= 0:
+        raise ValueError("Distance must be positive")
+    
+    return GRAVITATIONAL_CONSTANT * (mass1 * mass2) / distance**2
 
-    def calculate_coulomb_force(self, charge1, charge2, distance):
-        return ELEMENTARY_CHARGE * (charge1 * charge2) / distance**2
+def calculate_coulomb_force(charge1, charge2, distance):
+    if distance <= 0:
+        raise ValueError("Distance must be positive")
+        
+    return COULOMB_CONSTANT * abs(charge1 * charge2) / distance**2
 
-class UnitConverter:
-    def joules_to_calories(self, joules):
-        CALORIE_TO_JOULE = 4.184
-        return joules / CALORIE_TO_JOULE
+def joules_to_calories(joules):
+    JOULE_TO_CALORIE = 0.239006
+    return joules * JOULE_TO_CALORIE
 
-    def meters_to_kilometers(self, meters):
-        return meters / 1000
+def meters_to_kilometers(meters):
+    return meters / 1000
 
-class FormulaCalculator:
-    def calculate_energy(self, mass, velocity):
-        return 0.5 * mass * velocity**2
+def calculate_kinetic_energy(mass, velocity):
+    if mass < 0:
+        raise ValueError("Mass cannot be negative")
+        
+    return 0.5 * mass * velocity**2
